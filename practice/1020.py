5a160b2f9c0017c676602e1b93a9fe68012abd75
YangYuS8 <YangYuS8@163.com>
2025-11-03 17:30:35 +0800

    20251103

--- practice/1020.py
+++
@@ -1,8 +0,0 @@
-# 题目描述
-# 输入一个正整数n，输出1!+3!+5!+……+n!的和。
-import math
-n = int(input())
-s = 0
-for i in range(1, n+1, 2):
-    s += math.factorial(i)
-print(f"n = {n}, s = {s}")
--- practice/1027.py
+++
@@ -1,6 +0,0 @@
-# 题目描述
-# 一行输入若干名学生的身高，其中以空格分隔，每个身高都是一个正整数。
-# 计算他们的平均身高，输出超过平均身高的输入值，每个数后面有一个空格，输出的顺序和输入的相同。
-heights = list(map(int, input().split()))
-avg_height = sum(heights) / len(heights)
-print(' '.join(str(h) for h in heights if h > avg_height), end=" ")
